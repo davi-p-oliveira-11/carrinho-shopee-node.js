c64f6bfa574d534c3aea7179b41d3cdcdb2d4d96
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-01-19 20:50:24 -0300

    added cart.js functions

---
+++ src/services/cart.js
@@ -0,0 +1,15 @@
+// which actions my cart can do ?
+
+/* use cases */
+
+/* add item */
+async function addItem(userCart) {}
+
+// delete item
+async function deleteItem(userCart, name) {}
+
+// remove an item - 1 item
+async function removeItem(userCart, index) {}
+// calculate the total
+
+async function name(params) {}
