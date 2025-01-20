580049565ef82729882d5455541a91a1a16bb613
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-01-20 01:44:35 -0300

    added calculateTotal function

--- src/services/cart.js
+++ src/services/cart.js
@@ -3,7 +3,14 @@
 /* use cases */
 
 /* add item */
-async function addItem(userCart) {}
+async function addItem(userCart, item) {
+  userCart.push(item);
+}
+
+// calculate the total
+async function calculateTotal(userCart) {
+  return userCart.reduce((total, item) => total + item.subtotal(), 0);
+}
 
 // delete item
 async function deleteItem(userCart, name) {}
@@ -11,5 +18,4 @@ async function deleteItem(userCart, name) {}
 // remove an item - 1 item
 async function removeItem(userCart, index) {}
 
-// calculate the total
-async function calculateTotal(userCart) {}
+export { addItem, calculateTotal, deleteItem, removeItem };
