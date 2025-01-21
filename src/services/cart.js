fd2a75f69485d491ae59be2c7d5684efa54698dc
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-01-21 03:24:29 -0300

    remove item by index

--- src/services/cart.js
+++ src/services/cart.js
@@ -10,7 +10,7 @@ async function addItem(userCart, item) {
 // calculate the total
 async function calculateTotal(userCart) {
   const result = userCart.reduce((total, item) => total + item.subtotal(), 0);
-  console.log(`\n Total: ${result}`);
+  console.log(`\n Total: ${result.toFixed(2)}`);
 }
 
 // delete item
@@ -24,7 +24,12 @@ async function deleteItem(userCart, name) {
 
 // remove an item - 1 item
 async function removeItem(userCart, index) {
+  // change the visual index of the user, to backend index
+
+  const deleteIndex = index - 1;
+
   // if is bigger than the size of tne cart
+
   if (index >= 0 && index < userCart.length) {
     userCart.splice(index, 1);
   }
